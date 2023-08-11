7ea7f71b9140e4ac252ca4afb0e41db0e9484e57
Ajaykumarakk <Ajaykumar.kumarasamy@gmail.com>
2023-08-11 13:55:54 +0530

    Add project files.

---
+++ MVC in HTML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVC_in_HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Mvc/Controllers/DesignController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc.Controllers
+{
+    public class DesignController : Controller
+    {
+        // GET: DesignController
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: DesignController/Details/5
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        // GET: DesignController/Create
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        // POST: DesignController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit()
+        {
+            
+            {
+                return View();
+            }
+        }
+
+        // GET: DesignController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DesignController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DesignController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DesignController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
